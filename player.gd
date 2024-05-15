cf25335d6861106f3b7e7d67df6510d8310309cd
leon <leon.pireon00@gmail.com>
2024-05-15 19:29:08 +0200

    improved movement, jumps now don't lose momentum if directional key isn't pressed.

--- player.gd
+++ player.gd
@@ -1,8 +1,12 @@
 extends CharacterBody2D
 
 @export var run_speed = 450
+@export var air_speed_coefficient = 0.1
 @export var jump_force = -600
+@export var slide_boost = 200
 @export var gravity = 1200
+@export var MAX_SPEED_X = 500
+@export var MAX_SPEED_Y = 1000
 
 var screen_size
 var jumping = false
@@ -14,17 +18,27 @@ func _ready():
 	screen_size = get_viewport_rect().size
 
 func get_input():
-	velocity.x = 0
+	if !jumping:
+		velocity.x = 0
+	elif velocity.x > MAX_SPEED_X:
+		velocity.x = MAX_SPEED_X
+	elif velocity.x < MAX_SPEED_X * -1:
+		velocity.x = MAX_SPEED_X * -1
+	
 	var right = Input.is_action_pressed("right")
 	var left = Input.is_action_pressed("left")
-	var jump = Input.is_action_pressed("jump")
+	var jump = Input.is_action_just_pressed("jump")
 	
 	if jump and is_on_floor():
 		jumping = true
 		velocity.y = jump_force
-	if right:
+	if right and jumping:
+		velocity.x += run_speed * air_speed_coefficient
+	elif right:
 		velocity.x += run_speed
-	if left:
+	if left and jumping:
+		velocity.x -= run_speed * air_speed_coefficient 
+	elif left:
 		velocity.x -= run_speed
 
 func anim_handler():
