4f7d6019b1b87aca21e4b835b3d5e29e47fbc97e
leon <leon.pireon00@gmail.com>
2024-05-16 20:39:04 +0200

    added deceleration by ground friction

--- player.gd
+++ player.gd
@@ -2,28 +2,26 @@ extends CharacterBody2D
 
 @export var run_speed = 450
 @export var air_speed_coefficient = 0.1
+@export var ground_friction = 1500
 @export var jump_force = -600
 @export var slide_boost = 200
 @export var gravity = 1200
 @export var MAX_SPEED_X = 500
 @export var MAX_SPEED_Y = 1000
 
-var screen_size
 var jumping = false
 
 @onready var sprite = $AnimatedSprite2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	screen_size = get_viewport_rect().size
+@onready var screen_size = get_viewport_rect().size
 
 func get_input():
-	if !jumping:
-		velocity.x = 0
-	elif velocity.x > MAX_SPEED_X:
-		velocity.x = MAX_SPEED_X
-	elif velocity.x < MAX_SPEED_X * -1:
-		velocity.x = MAX_SPEED_X * -1
+	match velocity.x:
+		var x when (x > 0 and x < 1) or (x < 0 and x > -1):
+			velocity.x = 0
+		var x when x > MAX_SPEED_X:
+			velocity.x = MAX_SPEED_X
+		var x when velocity.x < MAX_SPEED_X * -1:
+			velocity.x = MAX_SPEED_X * -1
 	
 	var right = Input.is_action_pressed("right")
 	var left = Input.is_action_pressed("left")
@@ -32,12 +30,16 @@ func get_input():
 	if jump and is_on_floor():
 		jumping = true
 		velocity.y = jump_force
+	# moving right while jumping (not falling)
 	if right and jumping:
 		velocity.x += run_speed * air_speed_coefficient
+	# moving right while running
 	elif right:
 		velocity.x += run_speed
+	# moving left while jumping
 	if left and jumping:
 		velocity.x -= run_speed * air_speed_coefficient 
+	# moving left while running
 	elif left:
 		velocity.x -= run_speed
 
@@ -60,6 +62,10 @@ func _process(delta):
 		jumping = false
 	get_input()
 	velocity.y += gravity*delta
+	if velocity.x > 0 and !jumping:
+		velocity.x -= ground_friction * delta
+	elif velocity.x < 0 and !jumping:
+		velocity.x += ground_friction * delta
 	anim_handler()
 	
 	move_and_slide()
